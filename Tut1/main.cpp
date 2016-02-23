6b0c84163473e11d56788bdece7910e247aec2c8
randalx2 <pmisthry@gmail.com>
2016-02-23 23:48:09 +0200

    First Part

---
+++ Tut1/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstdio>
+#include <ctime> // Needed for srand function to seed the randomizer
+#include <string>
+#include <cmath>
+#include <cstdlib> //needed for random number generation in C++
+
+using namespace std;
+
+int main()
+{
+	srand(time(NULL)); //MUST START BY SEEDING THE RANDOMIZER IN THESE GAMES
+	int irand; // variable to hole the random number value
+	
+	irand = rand() % 20 + 1; //Generate the random number between 1 and 20
+	cout << "Random Number is " << irand << endl;
+
+	if (irand % 3 == 0)
+	{
+		cout << "fizz" << endl;
+	}
+
+	else if (irand % 5 == 0)
+	{
+		cout << "buzz" << endl;
+	}
+	else if ((irand % 3 == 0) && (irand % 5 == 0))
+	{
+		cout << "fizz buzz" << endl;
+	}
+	else {
+		cout << irand << " is not directly divisible by 3 or 5 try again" << endl;
+	}
+
+
+	system("PAUSE");
+	return 0;
+}
